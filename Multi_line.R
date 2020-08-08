ac5e83a5d85c8eac38b15e56567e5740a2fe9ced
Rishita Mukherjee <51188766+rishita2701@users.noreply.github.com>
2020-08-08 20:38:31 +0530

    create

---
+++ Multi_line.R
@@ -0,0 +1,8 @@
+R does not support multi-line comments but you can perform a trick which is something as follows:
+
+
+if(FALSE){
+"This is a demo for multi-line comments and it should be put
+inside either a single or the double quote"
+}
+
